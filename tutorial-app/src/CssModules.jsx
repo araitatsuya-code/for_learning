4f044ab97d9fde4bc92ffa6b814ce88f87448d6d
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2021-10-31 21:35:11 +0900

    p157

---
+++ tutorial-app/src/CssModules.jsx
@@ -0,0 +1,10 @@
+import classes from "./CssModules.module.scss";
+
+export const CssModules = () => {
+  return (
+    <div>
+      <p>CSS Moduleです</p>
+      <button>ボタン</button>
+    </div>
+  );
+};
